34e9a48d9ed84d67240f7f880fad8c58388f3c4a
unknown <pallavi.ambali429@gmail.com>
2022-10-20 10:13:31 +0200

    [itemis]: addressed decimal locale issue

--- src/main/java/bill/ReceiptGeneratorVisitor.java
+++ src/main/java/bill/ReceiptGeneratorVisitor.java
@@ -3,13 +3,18 @@ package bill;
 import product.*;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class ReceiptGeneratorVisitor implements Visitor {
 
-    DecimalFormat format = new DecimalFormat("0.00");
+    private final DecimalFormat formatter = ((DecimalFormat) NumberFormat.getCurrencyInstance(Locale.GERMANY));
+    private final String pattern = "#.##";
+    NumberFormat numberFormat = NumberFormat.getInstance(Locale.GERMANY);
     private Map<Item, Double> priceMap = new HashMap<>();
     private float salesTax = 0;
 
@@ -19,22 +24,36 @@ public class ReceiptGeneratorVisitor implements Visitor {
 
     @Override
     public void visitExemptedItem(ExemptedItem item) {
-        priceMap.put(item, Double.valueOf(format.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice())));
+        try {
+            formatter.applyPattern(pattern);
+            priceMap.put(item, numberFormat.parse(formatter.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice())).doubleValue());
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void visitNonExemptedItem(NonExemptedItem item) {
-        Double tax = roundToHalf(item.getShelfPrice() * item.getQuantity() * (item.getTaxRate()));
-
-        priceMap.put(item, Double.valueOf(format.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice() + tax)));
-        salesTax += tax;
+        try {
+            Double tax = roundToHalf(item.getShelfPrice() * item.getQuantity() * (item.getTaxRate()));
+            formatter.applyPattern(pattern);
+            priceMap.put(item, numberFormat.parse(formatter.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice() + tax)).doubleValue());
+            salesTax += tax;
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void visitImportedItem(ImportedItem item) {
-        Double tax = roundToHalf(item.getShelfPrice() * item.getQuantity() * (item.getTaxRate()));
-        priceMap.put(item, Double.valueOf(format.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice() + tax)));
-        salesTax += tax;
+        try {
+            Double tax = roundToHalf(item.getShelfPrice() * item.getQuantity() * (item.getTaxRate()));
+            formatter.applyPattern(pattern);
+            priceMap.put(item, numberFormat.parse(formatter.format(priceMap.getOrDefault(item, 0.0) + item.getQuantity() * item.getShelfPrice() + tax)).doubleValue());
+            salesTax += tax;
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void calculateAndGenerateReceipt(List<Item> items) {
@@ -45,13 +64,14 @@ public class ReceiptGeneratorVisitor implements Visitor {
     }
 
     public void generateReceipt(List<Item> items) {
+        formatter.applyPattern(pattern);
         items.forEach(item -> {
-                    System.out.println(item.getItemDescription() + ":" + format.format(priceMap.get(item)));
+                    System.out.println(item.getItemDescription() + ":" + formatter.format(priceMap.get(item)));
                 }
         );
         System.out.println("Sales taxes: " + (salesTax));
         System.out.println("Total: " + (
-                format.format(priceMap.values().stream().reduce(0.0, (a, b) -> a + b))));
+                formatter.format(priceMap.values().stream().reduce(0.0, (a, b) -> a + b))));
 
     }
 
